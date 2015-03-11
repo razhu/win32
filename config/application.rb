e6f1fbd14b5d8a859a927569d640ddb4880e74b2
ramiro <ramihertz@gmail.com>
2015-03-10 20:01:16 -0400

    modified application.rb

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module Casasmap
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Added by ramiro
+    config.assets.initialize_on_precompile = false
   end
 end
